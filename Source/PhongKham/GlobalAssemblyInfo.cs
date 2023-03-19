ab7650d4ff1e33d7a5aebb50431738d4c276169c
Ty nguyen <1212502student@gmail.com>
2023-03-19 18:43:48 +0700

    version bump

--- Source/PhongKham/GlobalAssemblyInfo.cs
+++ Source/PhongKham/GlobalAssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 [assembly: AssemblyCompany("Clinic")]
 [assembly: AssemblyProduct("PhongKham")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyVersion("1.9.1.0")]
-[assembly: AssemblyFileVersion("1.9.1.0")]
+[assembly: AssemblyVersion("1.9.2.0")]
+[assembly: AssemblyFileVersion("1.9.2.0")]
